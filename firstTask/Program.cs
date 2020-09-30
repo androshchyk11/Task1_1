41fe8aeca9b556037e0fc4073a40812099543d68
artemmar <artemmarcoop@gmail.com>
2020-09-30 12:48:58 +0300

    wrong word fixed

--- firstTask/Program.cs
+++ firstTask/Program.cs
@@ -6,7 +6,7 @@ namespace firstTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привіт ДонНу!");
         }
     }
 }
